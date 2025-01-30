93848788e07793239dd007b6bd3c56d36a4214a5
Reulyson <reulyson@gmail.com>
2025-01-29 23:14:48 -0400

    feat(templates): Move index.html para galeria e atualiza views.py

--- galeria/views.py
+++ galeria/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render  # Importa a função render para renderizar
 
 def index(request):
    
-    return render(request, 'index.html') # Renderiza o template 'index.html'
+    return render(request, 'galeria/index.html') # Renderiza o template 'index.html'
