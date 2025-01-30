170f23b5fecada83b4ebb1928b4de4026d83d72d
Reulyson <reulyson@gmail.com>
2025-01-30 09:00:20 -0400

    feat: Implementação da página de imagemm

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path # Importa a função 'path' para definir rotas
 from galeria.views import index  # Importa a função 'index' da view
+from galeria.views import imagem# Importa a função 'imagem' da view
 
 urlpatterns = [ # Define uma lista de padrões de URL
     path('', index), # Rota para a URL raiz ('/'), que chama a função 'index'
+    path('imagem/', imagem), # Rota para a URL raiz ('imagem/'), que chama a função 'imagem'
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render  # Importa a função render para renderizar
 
 def index(request):
    
-    return render(request, 'galeria/index.html') # Renderiza o template 'index.html'
+    return render(request, 'galeria/index.html') #Renderiza o template 'index.html'
+
+def imagem(request):
+
+    return render(request, 'galeria/imagem.html') #Renderiza o template 'imagem.html'
