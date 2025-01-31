50e41800067674e3c11c6938c00267fb1d104b54
Reulyson <reulyson@gmail.com>
2025-01-31 08:23:33 -0400

    flix(templates): corrigindo a navegação entre a página principal e a página de imagem

--- galeria/urls.py
+++ galeria/urls.py
@@ -3,6 +3,6 @@ from galeria.views import index  # Importa a função 'index' da view
 from galeria.views import imagem# Importa a função 'imagem' da view
 
 urlpatterns = [ # Define uma lista de padrões de URL
-    path('', index), # Rota para a URL raiz ('/'), que chama a função 'index'
-    path('imagem/', imagem), # Rota para a URL raiz ('imagem/'), que chama a função 'imagem'
+    path('', index, name='index'), # Rota para a URL raiz ('/'), que chama a função 'index'
+    path('imagem/', imagem, name='imagem'), # Rota para a URL raiz ('imagem/'), que chama a função 'imagem'
 ]
