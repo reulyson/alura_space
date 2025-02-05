615418dab0ee54e78cc0b8197fd490a515c6ad5b
Reulyson <reulyson@gmail.com>
2025-02-05 15:09:59 -0400

    feat: implementar exibição dinâmica de imagens na galeria

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render  # Importa a função render para renderizar templates
 
 def index(request):
+
+    # criando um dicionario com informaçãoes para os cards do index
+    dados = {
+    1: {"nome": "Nebulosa de Carina",
+        "legenda": "webbtelescope.org / NASA / James Webb"},
+    2: {"nome": "Galáxia NGC 1079",
+        "legenda": "nasa.org / NASA / Hubble"}
+}
    
-    return render(request, 'galeria/index.html') #Renderiza o template 'index.html'
+    return render(request, 'galeria/index.html', {"cards": dados}) #Renderiza o template 'index.html' e carrega as informações do dicionário 'dados'
 
 def imagem(request):
 
