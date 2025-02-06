0ca8412aa08b0ee2c56e547aba2f250f7c649bd0
Reulyson <reulyson@gmail.com>
2025-02-06 08:52:05 -0400

    feat: criar modelo Fotografia com campos nome, legenda, descricao e foto

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model): # Estamos criando uma nova classe chamada Fotografia que herda de models.Model
+    # adicionando campos ao modelo.
+    nome = models.CharField(max_length=100, null=False, blank=False) #  Usamos CharField porque queremos armazenar uma string
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False) # usando TextField por que podemos ter textos longos
+    foto = models.CharField(max_length=150, null=False, blank=False) # null=False significa que esse campo não pode ser nulo , e blank=False significa que não pode ser deixado em branco em formulários.
+
+def __str__(self):
+    return f"Fotografia [nome=[self.nome]]" # Este método define como o objeto Fotografia será representado como uma string. Isso é útil para facilitar a visualização dos objetos no Django Admin ou em outros lugares.
