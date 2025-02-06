3d98cf459ea516aad26d3d360c14589e30bb9169
Reulyson <reulyson@gmail.com>
2025-02-06 09:47:04 -0400

    feat: adicionar funcionalidade de criação e salvamento de fotografias no banco de dados

--- galeria/models.py
+++ galeria/models.py
@@ -8,4 +8,4 @@ class Fotografia(models.Model): # Estamos criando uma nova classe chamada Fotogr
     foto = models.CharField(max_length=150, null=False, blank=False) # null=False significa que esse campo não pode ser nulo , e blank=False significa que não pode ser deixado em branco em formulários.
 
 def __str__(self):
-    return f"Fotografia [nome=[self.nome]]" # Este método define como o objeto Fotografia será representado como uma string. Isso é útil para facilitar a visualização dos objetos no Django Admin ou em outros lugares.
+    return f"Fotografia [nome={self.nome}]" # Este método define como o objeto Fotografia será representado como uma string. Isso é útil para facilitar a visualização dos objetos no Django Admin ou em outros lugares.
