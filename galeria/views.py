d9283e5711f0a9b65160bf088bc4cb45b87fd1ca
Reulyson <reulyson@gmail.com>
2025-02-07 08:24:58 -0400

    feat: Implementa a exibição dinâmica de fotografias na galeria
    - Atualiza a função index no views.py para capturar todas as fotografias do banco de dados.
    - Modifica o template index.html para exibir as fotografias de forma dinâmica, utilizando a estrutura de repetição.
    - Adiciona verificação condicional para garantir que a galeria não exiba cards vazios.
    - Atualiza os links das imagens para redirecionar corretamente para a página de detalhes da fotografia.
    - Melhora a legibilidade do código com comentários explicativos.
    
    Essa implementação garante que as fotografias sejam carregadas corretamente a partir do banco de dados e exibidas na interface do usuário.

--- galeria/views.py
+++ galeria/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render  # Importa a função render para renderizar templates
+from galeria.models import Fotografia
 
 def index(request):
 
-    # criando um dicionario com informaçãoes para os cards do index
-    dados = {
-    1: {"nome": "Nebulosa de Carina",
-        "legenda": "webbtelescope.org / NASA / James Webb"},
-    2: {"nome": "Galáxia NGC 1079",
-        "legenda": "nasa.org / NASA / Hubble"}
-}
-   
-    return render(request, 'galeria/index.html', {"cards": dados}) #Renderiza o template 'index.html' e carrega as informações do dicionário 'dados'
+    fotografias = Fotografia.objects.all() # variavel criada para exibir os objetos referentes do banco de dados
+
+    return render(request, 'galeria/index.html', {"cards": fotografias}) #Renderiza o template 'index.html' e carrega as informações do banco de dados refrentes as fotos
 
 def imagem(request):
 
