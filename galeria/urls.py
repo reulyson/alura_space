b1c79c4dd23986c8925bd70235a8964bd45db2f9
Reulyson <reulyson@gmail.com>
2025-02-07 08:24:58 -0400

    feat: Adiciona funcionalidade de upload e exibição dinâmica de fotografias
    - Implementa a adição de novas fotografias ao banco de dados através do shell do Django.
    - Cria rotas no arquivo 'urls.py' para a página inicial e para a exibição de fotografias individuais.
    - Atualiza a view 'imagem' em 'views.py' para capturar o ID da fotografia e retornar o objeto correspondente ou um erro 404 se não encontrado.
    - Modifica o template 'imagem.html' para exibir dinamicamente a imagem, título e legenda da fotografia selecionada.
    - Adiciona a imagem 'Galáxia NGC 1079' ao diretório de desenvolvimento para testes.
    
    Essa implementação melhora a navegação na galeria de imagens, permitindo que os usuários visualizem informações específicas de cada fotografia.

--- galeria/urls.py
+++ galeria/urls.py
@@ -3,6 +3,6 @@ from galeria.views import index  # Importa a função 'index' da view
 from galeria.views import imagem# Importa a função 'imagem' da view
 
 urlpatterns = [ # Define uma lista de padrões de URL
-    path('', index, name='index'), # Rota para a URL raiz ('/'), que chama a função 'index'
-    path('imagem/', imagem, name='imagem'), # Rota para a URL raiz ('imagem/'), que chama a função 'imagem'
+    path('', index, name='index'), # Rota para a página inicial
+    path('imagem/<int:foto_id>', imagem, name='imagem'), # Rota para a página da imagem, recebendo o id da foto
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,17 +1,12 @@
-from django.shortcuts import render  # Importa a função render para renderizar templates
+from django.shortcuts import render, get_object_or_404 # Importa a função render para renderizar templates e get_object_or_404  o objeto do banco de dado pelo seu id ou gerar o código 404 (não encontrado)
+from galeria.models import Fotografia
 
 def index(request):
 
-    # criando um dicionario com informaçãoes para os cards do index
-    dados = {
-    1: {"nome": "Nebulosa de Carina",
-        "legenda": "webbtelescope.org / NASA / James Webb"},
-    2: {"nome": "Galáxia NGC 1079",
-        "legenda": "nasa.org / NASA / Hubble"}
-}
-   
-    return render(request, 'galeria/index.html', {"cards": dados}) #Renderiza o template 'index.html' e carrega as informações do dicionário 'dados'
+    fotografias = Fotografia.objects.all() # Busca todas as fotografias no banco de dados
+    return render(request, 'galeria/index.html', {"cards": fotografias})  # Renderiza a página inicial com as fotografias
 
-def imagem(request):
-
-    return render(request, 'galeria/imagem.html') #Renderiza o template 'imagem.html'
+def imagem(request, foto_id):
+    
+    fotografia = get_object_or_404(Fotografia, pk=foto_id) # Captura a fotografia com o ID fornecido ou retorna 404 se não encontrar
+    return render(request, 'galeria/imagem.html', {"fotografia":fotografia}) # Renderiza a página da imagem com o objeto da fotografia
