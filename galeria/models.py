05cfcbf5ea10ae704cd1fba5eef6090faef47de1
Reulyson <reulyson@gmail.com>
2025-02-11 20:43:27 -0400

    feat: adicionar funcionalidade de publicação de fotografias no admin
    - Adicionado campo 'publicada' no model Fotografia
    - Atualizada a view index para filtrar fotografias publicadas
    - Modificado admin.py para exibir e editar o campo 'publicada' diretamente na lista

--- galeria/models.py
+++ galeria/models.py
@@ -15,6 +15,7 @@ class Fotografia(models.Model): # Estamos criando uma nova classe chamada Fotogr
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False) # usando TextField por que podemos ter textos longos
     foto = models.CharField(max_length=150, null=False, blank=False) # null=False significa que esse campo não pode ser nulo , e blank=False significa que não pode ser deixado em branco em formulários.
+    publicada = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Fotografia [nome={self.nome}]" # Este método define como o objeto Fotografia será representado como uma string. Isso é útil para facilitar a visualização dos objetos no Django Admin ou em outros lugares.0
--- galeria/views.py
+++ galeria/views.py
@@ -3,7 +3,7 @@ from galeria.models import Fotografia
 
 def index(request):
 
-    fotografias = Fotografia.objects.all() # variavel criada para exibir os objetos referentes do banco de dados
+    fotografias = Fotografia.objects.filter(publicada=True) # variavel criada para exibir os objetos referentes do banco de dados
 
     return render(request, 'galeria/index.html', {"cards": fotografias}) #Renderiza o template 'index.html' e carrega as informações do banco de dados refrentes as fotos
 
